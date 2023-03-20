5c7953cde4f95aa71a123ac8d9c7885b39fd3ca7
Finn Mattis <planto73@harkenpretty.com>
2023-03-20 07:37:21 -0400

    moved sentences page into sentences/freediff and sentences/index now serves to redirect to sentences/browse and sentences/freediff

--- src/pages/sentences/index.jsx
+++ src/pages/sentences/index.jsx
@@ -1,67 +1,31 @@
-import { faCodeCompare, faPen } from "@fortawesome/free-solid-svg-icons"
+import { faCompass, faPen } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react"
-import DiffDisplay from "../../components/diffdisplay"
-import TextField from "../../components/textfield"
-import diff from "../../lib/diff"
+import Link from "next/link"
 
 export default function index() {
-    const [text1, setText1] = useState("")
-    const [text2, setText2] = useState("")
-
-    const [editing, setEditing] = useState(true)
-    const [diffLines, setDiffLines] = useState([])
-
-    const onClick = () => {
-        if (editing) {
-            setDiffLines(diff(text1, text2))
-            setEditing(false)
-        } else {
-            setEditing(true)
-        }
-    }
-
     return (
-        <div className="flex min-h-[90vh] flex-col items-center bg-primary">
+        <div className="flex h-[90vh] flex-col items-center bg-primary">
             <p className="mt-6 text-6xl text-white">Practice Sentences</p>
-            <div className="flex w-full justify-evenly pt-8">
-                {editing ? (
-                    <TextField
-                        width={45}
-                        value={text1}
-                        onChange={(e) => {
-                            setText1(e.target.value)
-                        }}
-                    />
-                ) : (
-                    <DiffDisplay
-                        width={45}
-                        diffLines={diffLines}
-                        additions={false}
-                    />
-                )}
-                {editing ? (
-                    <TextField
-                        width={45}
-                        value={text2}
-                        onChange={(e) => {
-                            setText2(e.target.value)
-                        }}
-                    />
-                ) : (
-                    <DiffDisplay
-                        width={45}
-                        diffLines={diffLines}
-                        additions={true}
-                    />
-                )}
+            <div className="mt-6 flex w-[80%] justify-evenly sm:justify-around">
+                <Link href="/sentences/browse">
+                    <div className="flex aspect-[2/3] w-80 flex-col items-center justify-center rounded-lg bg-secondary drop-shadow-2xl sm:w-48">
+                        <p className="text-6xl text-primary">Browse</p>
+                        <FontAwesomeIcon
+                            icon={faCompass}
+                            className="mt-6 text-6xl text-primary"
+                        />
+                    </div>
+                </Link>
+                <Link href="/sentences/freediff">
+                    <div className="flex aspect-[2/3] w-80 flex-col items-center justify-center rounded-lg bg-secondary drop-shadow-2xl sm:w-48">
+                        <p className="text-6xl text-primary">Free Diff</p>
+                        <FontAwesomeIcon
+                            icon={faPen}
+                            className="mt-6 text-6xl text-primary"
+                        />
+                    </div>
+                </Link>
             </div>
-            <button
-                onClick={onClick}
-                className="mt-4 h-16 w-16 rounded-full border-2 border-secondary text-3xl text-secondary"
-            >
-                <FontAwesomeIcon icon={editing ? faCodeCompare : faPen} />
-            </button>
         </div>
     )
 }
