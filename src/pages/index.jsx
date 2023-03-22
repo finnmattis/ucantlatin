00251e89689f6bb1982350393e5e69290a3d8719
Finn Mattis <planto73@harkenpretty.com>
2023-03-22 08:59:20 -0400

    Basic landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,72 +1,10 @@
 export default function Home() {
     return (
-        <main>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Veritatis beatae minima reiciendis, fugit eligendi accusamus
-                odio ipsam. Repellat architecto ab distinctio porro ad,
-                provident ratione praesentium nesciunt est dicta quos nisi odio
-                esse cupiditate vero, sequi nostrum blanditiis laboriosam?
-                Repellendus tenetur harum dolor quae itaque, modi iste corporis
-                voluptatem ea blanditiis. Maxime blanditiis itaque impedit culpa
-                tempora id quidem molestiae! Cupiditate molestiae repellendus ut
-                explicabo quasi esse, sequi nesciunt eum debitis repudiandae ex
-                reprehenderit recusandae harum aperiam praesentium alias
-                incidunt magnam cum facere aut aspernatur, enim exercitationem.
-                Corporis fugit doloremque, eligendi cum dolore saepe ducimus
-                repellat, quod accusantium ratione cupiditate! Similique maxime
-                hic beatae consequatur, nulla explicabo quia molestias itaque
-                iste voluptatem deserunt perferendis modi debitis nemo
-                reiciendis fugit praesentium exercitationem nostrum voluptas
-                aliquid doloribus ad magnam expedita. Possimus, commodi
-                mollitia! Libero modi eius ipsum debitis, obcaecati perspiciatis
-                distinctio commodi? Libero in facere esse velit qui! Sit
-                perferendis laudantium, hic libero nam tempore omnis possimus
-                odio eius. Aspernatur ab culpa, obcaecati excepturi, quaerat
-                quas magni cum laboriosam debitis, nesciunt necessitatibus autem
-                molestias libero beatae laborum iure ullam rerum saepe
-                blanditiis sint! Esse id ipsam nam amet sed modi officia aperiam
-                vero, accusamus similique veniam quaerat, beatae quia illo quod
-                impedit unde. Tempore possimus pariatur corporis at obcaecati
-                quidem laudantium aut! Officiis mollitia praesentium iure nobis
-                voluptatum, dolores nulla asperiores ea laboriosam omnis
-                ratione. Reiciendis mollitia rerum vitae perferendis iusto
-                possimus suscipit, explicabo animi, quia, consequatur
-                dignissimos rem magni? Qui eos amet quibusdam quas. Error, amet
-                repellendus, accusamus saepe molestiae officiis quasi dolores
-                enim similique quia quisquam autem dolore reiciendis impedit
-                esse veniam quibusdam expedita sed maiores. Blanditiis sed
-                consectetur vero possimus dolores repellat minus quas autem,
-                magni sequi dolorum sapiente excepturi, amet voluptatum eum.
-                Officia sunt consequuntur veniam mollitia cumque quo soluta
-                recusandae, autem distinctio iusto. Nemo reiciendis, doloremque
-                esse qui cupiditate unde facere inventore. Animi, alias
-                accusamus incidunt, esse expedita nesciunt maiores magni eos
-                voluptates ipsam libero distinctio, nostrum minima fugit
-                reiciendis pariatur vel blanditiis ad vitae porro velit
-                architecto! Recusandae officiis modi magnam cum dolorum sunt
-                dignissimos mollitia repudiandae eligendi, exercitationem
-                deserunt porro veniam. Praesentium possimus laboriosam aut eos
-                eligendi rem dicta rerum repudiandae quo. Voluptas fugit illo
-                odio magnam optio amet ea ex ducimus eligendi placeat,
-                temporibus rem quis in tempora quasi possimus nam nemo saepe
-                repellendus ratione maxime assumenda esse. Vitae quis eum ea
-                illo accusamus eaque libero consectetur sequi, assumenda
-                quisquam porro nesciunt soluta pariatur voluptates quam
-                explicabo optio delectus at culpa id ipsa esse itaque
-                necessitatibus nulla! Pariatur adipisci ullam, fugiat magni
-                numquam laborum ea quisquam ipsa ipsum ducimus! Modi nisi quasi
-                atque ullam doloremque obcaecati fuga quis ab quas voluptas,
-                fugiat temporibus incidunt numquam tenetur tempora
-                exercitationem cupiditate ratione, in dicta, at minima! Autem
-                assumenda eos reiciendis, veniam doloribus, minus adipisci
-                consequuntur ex vel nesciunt provident quae necessitatibus magni
-                molestiae vitae quisquam est at iure, sit non laborum
-                voluptatibus? Neque accusantium illum labore porro alias saepe
-                impedit? Ex molestiae quaerat voluptate, accusamus magnam
-                ratione, eligendi, ea harum dolore animi assumenda perspiciatis.
-                Sunt, dignissimos!
-            </div>
-        </main>
+        <div className="flex h-[90vh] flex-col items-center bg-primary">
+            <p className="mt-6 text-6xl text-white">Welcome to UcantLatin</p>
+            <p className="mt-6 text-3xl text-white">
+                Practice terms and sentences or look up a word in the dictionary
+            </p>
+        </div>
     )
 }
